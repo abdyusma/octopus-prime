466e9e3999b2995e84d33c81a7b2ae5837d96c0d
abdyusma <111057405+abdyusma@users.noreply.github.com>
2022-09-11 14:18:30 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "core" {}
+
+module "caf-enterprise-scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "2.3.1"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+}
