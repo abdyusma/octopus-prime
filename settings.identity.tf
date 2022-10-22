586225f88cc2aa15588f52edd73de8723453b845
abdyusma <111057405+abdyusma@users.noreply.github.com>
2022-10-22 20:37:50 +0800

    Update settings.identity.tf

--- settings.identity.tf
+++ settings.identity.tf
@@ -5,9 +5,9 @@ locals {
       identity = {
         enabled = true
         config = {
-          enable_deny_public_ip             = true
-          enable_deny_rdp_from_internet     = true
-          enable_deny_subnet_without_nsg    = true
+          enable_deny_public_ip             = false
+          enable_deny_rdp_from_internet     = false
+          enable_deny_subnet_without_nsg    = false
           enable_deploy_azure_backup_on_vms = false
         }
       }
